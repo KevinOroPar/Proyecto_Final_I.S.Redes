eacb46e36a529e5f304cf007a2a18724c9092a04
KevinOroPar <kevin_chiva@hotmail.es>
2023-11-13 12:31:07 -0600

    regex manage

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import asyncio
 from os import write
 from dotenv import load_dotenv
 from Crypto.Cipher import AES
-import os
+import os, re
 
 load_dotenv()
 
@@ -13,6 +13,7 @@ if not shared_key:
     raise ValueError("La variable de entorno SHARED_KEY no está definida en el archivo .env")
 encoded_key = shared_key.encode()
 trunk_encoded_key = encoded_key[:16]
+patron = re.compile(r'.*Salir$')
 
 async def run_client() -> None:
 
@@ -20,13 +21,15 @@ async def run_client() -> None:
 
         while True:
             data = await reader.read(1024)
-            if not data:
-                print("conexión rechazada por el servidor")
-                break
             cipher = AES.new(trunk_encoded_key, AES.MODE_EAX, nonce=data[:16])
             message = cipher.decrypt(data[16:])
             msg = message.decode()
-            if msg == "Salir":
+            ismatch = patron.search(msg)
+            if ismatch:
+                writer.close()
+                await writer.wait_closed()
+                print("Conexión cerrada por el servidor =(")
+                print(msg[0:-5])
                 break
             resp = input(msg)
             cipher = AES.new(trunk_encoded_key,AES.MODE_EAX)
